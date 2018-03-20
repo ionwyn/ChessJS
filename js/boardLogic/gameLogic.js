75c214ade9c2cbb695d6ad84008fe93dc7ba1db8
ionwyn <ionwynsean@gmail.com>
2018-03-19 19:35:37 -0700

    Redesign Zobrist Table

--- js/boardLogic/gameLogic.js
+++ js/boardLogic/gameLogic.js
@@ -193,9 +193,15 @@ var minimax = function (depth, game, alpha, beta, isMaximisingPlayer) {
     if (isMaximisingPlayer) {
         var bestMove = -9999;
         for (var i = 0; i < newGameMoves.length; i++) {
+
+						hashValue ^= ZobristTable
+
             game.ugly_move(newGameMoves[i]);
             bestMove = Math.max(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
             game.undo();
+
+
+
             alpha = Math.max(alpha, bestMove);
             if (beta <= alpha) {
                 return bestMove;
